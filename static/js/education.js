9894a55b3b727611563d23aceb9d589c57f7ab47
byfarm <buckshootroger@gmail.com>
2024-10-01 08:09:16 -0700

    jj

--- static/js/education.js
+++ static/js/education.js
@@ -1,19 +1,21 @@
-let table = [
-    ["Computer Methods for Engineers", "Roy J Johnson Memorial Scholarship"],
-    ["Advanced Engineering Math", "Gonzaga Deans Scholarship"],
-    ["Controls Engineering", "Gonzaga Leaders Scholarship"],
-    ["Thermodynamics", ""],
-    ["Machine Design", ""],
-    ["Heat Transfer", ""],
-    ["Material Engineering", ""]
-];
+function fill_table() {
+    let table = [
+        ["Computer Methods for Engineers", "Roy J Johnson Memorial Scholarship"],
+        ["Advanced Engineering Math", "Gonzaga Deans Scholarship"],
+        ["Controls Engineering", "Gonzaga Leaders Scholarship"],
+        ["Thermodynamics", ""],
+        ["Machine Design", ""],
+        ["Heat Transfer", ""],
+        ["Material Engineering", ""]
+    ];
 
-var eduTable = $("#education-table");
-for (let i = 0; i < table.length; i++) {
-    var tr = $("<tr>");
-    for (let j = 0; j < table[i].length; j++) {
-        var td1Tag = $("<td>").text(table[i][j]);
-        td1Tag.appendTo(tr);
+    var eduTable = $("#education-table");
+    for (let i = 0; i < table.length; i++) {
+        var tr = $("<tr>");
+        for (let j = 0; j < table[i].length; j++) {
+            var td1Tag = $("<td>").text(table[i][j]);
+            td1Tag.appendTo(tr);
+        }
+        tr.appendTo(eduTable);
     }
-    tr.appendTo(eduTable);
 }
